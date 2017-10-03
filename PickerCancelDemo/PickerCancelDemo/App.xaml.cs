bc3216c8d633d10bc1eaed716d3248bd93920dcc
Sanya <sanya2371@gmail.com>
2017-10-03 19:42:39 +0300

    Add project files.

--- PickerCancelDemo/PickerCancelDemo/App.xaml.cs
+++ PickerCancelDemo/PickerCancelDemo/App.xaml.cs
@@ -12,7 +12,7 @@ namespace PickerCancelDemo
         {
             InitializeComponent();
 
-            NavigationService.NavigateAsync("NavigationPage/MainPage?title=Hello%20from%20Xamarin.Forms");
+            NavigationService.NavigateAsync("NavigationPage/MainPage");
         }
 
         protected override void RegisterTypes()
